e0fd94a104b3ca7bb54731b12f93cbd0cf4511f3
Mishka <mishkachargazia@gmail.com>
2020-04-22 19:54:33 +0400

    final

--- company_project/company.playground/Contents.swift
+++ company_project/company.playground/Contents.swift
@@ -1,24 +1,85 @@
 import Cocoa
 
 class Company {
-    var name: String { return  "" }
+    var name: String
     var date: Int
     var headQuarter: String
     var founders: [String]
-    
-    init() {
-        self.date = 2000
-        self.headQuarter = "California"
-        self.founders = ["Steve Jobs"]
+    var products: [String]
+
+    init(name: String, date: Int, headQuarter: String, founders: [String], products: [String] = ["No Products"] ) {
+        self.name = name
+        self.date = date
+        self.headQuarter = headQuarter
+        self.founders = founders
+        self.products = products
     }
     
+    func printName() {
+        print(self.name)
+    }
+    
+    func printDate() {
+        print(self.date)
+    }
+    
+    func printHeadQuarter() {
+        print(self.headQuarter)
+    }
+    
+    func printFounders() {
+        print(self.founders.joined(separator: ", "))
+    }
+    
+    func printProducts() {
+        print(self.products.joined(separator: ", "))
+    }
 }
 
 class Apple: Company {
-    override var name: String { return "Apple" }
-    
+    init() {
+        super.init(name: "Apple", date: 1976, headQuarter: "Cupertino, California", founders: ["Steve Jobs", "Steve Wozniak", "Ronald Wayne"],
+                   products: ["iPhone X", "Macbook Air", "Swift"])
+    }
 }
 
-var apple1 = Apple()
+class Google: Company {
+    init() {
+        super.init(name: "Google", date: 1998, headQuarter: "Mountain View, California", founders: ["Larry Page", "Sergey Brin"],
+        products: ["Android", "Pixel", "Chrome"])
+    }
+}
 
-apple1.name
+class Facebook: Company {
+    init() {
+        super.init(name: "Facebook", date: 2004, headQuarter: "Menlo Park, California", founders: ["Mark Zuckerberg", "Steve Wozniak", "Ronald Wayne"],
+        products: ["Messenger", "Instagram"])
+    }
+}
+
+var apple = Apple()
+
+apple.printName()
+apple.printDate()
+apple.printHeadQuarter()
+apple.printFounders()
+apple.printProducts()
+print("-----")
+
+var facebook = Facebook()
+
+facebook.printName()
+facebook.printDate()
+facebook.printHeadQuarter()
+facebook.printFounders()
+facebook.printProducts()
+print("-----")
+
+
+var google = Google()
+
+google.printName()
+google.printDate()
+google.printHeadQuarter()
+google.printFounders()
+google.printProducts()
