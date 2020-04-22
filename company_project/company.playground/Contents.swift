b0769c6ed023191077d9535de3d2bf29e1f047ff
Mishka <mishkachargazia@gmail.com>
2020-04-22 18:40:08 +0400

    comp

--- company_project/company.playground/Contents.swift
+++ company_project/company.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
-var str = "Hello, playground"
+class Company {
+    var name: String { return  "" }
+    var date: Int
+    var headQuarter: String
+    var founders: [String]
+    
+    init() {
+        self.date = 2000
+        self.headQuarter = "California"
+        self.founders = ["Steve Jobs"]
+    }
+    
+}
+
+class Apple: Company {
+    override var name: String { return "Apple" }
+    
+}
+
+var apple1 = Apple()
+
+apple1.name
