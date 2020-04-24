b19252e50ed1f27b2b3af5b569e8f3b759fc2d15
Mishka <mishkachargazia@gmail.com>
2020-04-24 15:14:30 +0400

    square-final

---
+++ square/square/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  square
+//
+//  Created by Admin on 4/24/20.
+//  Copyright © 2020 Mishka TBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
