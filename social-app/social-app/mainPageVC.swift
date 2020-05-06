69a896e1812993c424dfde59200f9a84b514545a
Mishka <mishkachargazia@gmail.com>
2020-05-06 17:37:08 +0400

    f-social

--- social-app/social-app/mainPageVC.swift
+++ social-app/social-app/mainPageVC.swift
@@ -71,6 +71,10 @@ class mainPageVC: UIViewController {
 
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
